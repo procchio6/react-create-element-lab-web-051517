73bbc1cd8d9b4d71d790301540e96d87a8a7d447
Pat Rocchio <procchio6@gmail.com>
2017-07-17 09:45:44 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // IMPORTANT!!!! uncomment the lines below to make your tests work
-/*
+
+const title = React.createElement('h1', {}, 'An Awesome Person')
+const tagline = React.createElement('p', {}, 'Who is learning React')
+const interests = ['JavaScript', 'React', 'Movies', 'Ice cream'].map((interest) => {
+  return React.createElement('li', {}, interest)
+})
+const interestList = React.createElement('ul', {className:'my-interests'}, interests)
+const meInReact = React.createElement('div', {className:'me'}, [title, tagline, interestList])
+
+
+
 ReactDOM.render(
-  meInReact, 
+  meInReact,
   document.getElementById('root')
 );
 
 export default meInReact
-*/
